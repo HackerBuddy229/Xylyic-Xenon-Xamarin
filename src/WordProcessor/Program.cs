25a2195f53493f61ccf746cfdfcaa90d32f9fc52
Rasmus Bengtsson <rasmusb229@gmail.com>
2021-01-07 18:19:33 +0100

    modified words with Begining upper

--- src/WordProcessor/Program.cs
+++ src/WordProcessor/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
 
 namespace WordProcessor
 {
@@ -6,7 +11,48 @@ namespace WordProcessor
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            const string filename = "nouns.list";
+            var words = GetWords(filename);
+
+            words = words.Select(ModifyWord).ToList();
+
+            WriteWords(filename, words);
         }
+
+        private static void WriteWords(string filename, List<string> words)
+        {
+            using var stream = new FileStream(filename, FileMode.Create);
+            using var sw = new StreamWriter(stream);
+
+            words.ForEach(sw.WriteLine);
+        }
+
+        private static List<string> GetWords(string filename)
+        {
+            var words = new List<string>();
+            using var stream = new FileStream(filename, FileMode.Open);
+            using var sr = new StreamReader(stream);
+            while (true)
+            {
+                var nextWord = sr.ReadLine();
+                if (nextWord == null)
+                    break;
+
+                words.Add(nextWord);
+            }
+
+            return words;
+        }
+
+        private static string ModifyWord(string word)
+        {
+            var endingLetters = word.TakeLast(word.Length - 1);
+            var builder = new StringBuilder();
+            foreach (var letter in endingLetters)
+                builder.Append(letter);
+
+            return word[0].ToString().ToUpper() + builder.ToString();
+        }
+
     }
 }
